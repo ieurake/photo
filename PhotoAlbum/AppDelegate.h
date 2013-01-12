27ffbe5916191736d13446ab20f4ff3532881a1e
minzhaoyuan <minzhaoyuan@gmail.com>
2013-01-13 01:20:41 +0800

    Initial Commit

---
+++ PhotoAlbum/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PhotoAlbum
+//
+//  Created by 1排右1Mctu on 13-1-13.
+//  Copyright (c) 2013年 Mctu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PhotoAlbum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoAlbum
+//
+//  Created by 1排右1Mctu on 13-1-13.
+//  Copyright (c) 2013年 Mctu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
